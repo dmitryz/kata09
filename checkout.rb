044760e48130eced9caec49115456aef4f8fe3fd
Dmitry <dmitry@yulchik.com>
2018-05-19 22:40:46 +0200

    Refactor price_rules

--- checkout.rb
+++ checkout.rb
@@ -6,10 +6,10 @@ require_relative('price_rules/normal')
 require_relative('price_rules/discount')
 
 RULES = PriceRules::Schema.build do
-  discount_price sku: 'A', amount: 50, discount_on: 3, discount_price: 130
-  discount_price sku: 'B', amount: 30, discount_on: 2, discount_price: 45
-  price sku: 'C', amount: 20
-  price sku: 'D', amount: 15
+  discount sku: 'A', amount: 50, discount_on: 3, discount_price: 130
+  discount sku: 'B', amount: 30, discount_on: 2, discount_price: 45
+  normal sku: 'C', amount: 20
+  normal sku: 'D', amount: 15
 end
 
 class CheckOut
