0e43d327ac57e229e6ee41486dc9f8e6246c544e
Dmitry <dmitry@yulchik.com>
2018-05-19 22:39:50 +0200

    Refactor price_rules

--- checkout.rb
+++ checkout.rb
@@ -1,14 +1,16 @@
 require_relative('cart')
 require_relative('item')
 require_relative('price_calculator')
+require_relative('price_rules/schema')
 require_relative('price_rules/normal')
 require_relative('price_rules/discount')
 
-RULES = { 'A' => PriceRules::Discount.new(price: 50, discount_on: 3, discount_price: 130),
-          'B' => PriceRules::Discount.new(price: 30, discount_on: 2, discount_price: 45),
-          'C' => PriceRules::Normal.new(price: 20),
-          'D' => PriceRules::Normal.new(price: 15)
-        }.freeze
+RULES = PriceRules::Schema.build do
+  discount_price sku: 'A', amount: 50, discount_on: 3, discount_price: 130
+  discount_price sku: 'B', amount: 30, discount_on: 2, discount_price: 45
+  price sku: 'C', amount: 20
+  price sku: 'D', amount: 15
+end
 
 class CheckOut
   def initialize(rules)
